63d020096a83520452900d9698c0eef2ead51877
ThiagoRibeiro16 <thiago.escola321@gmail.com>
2025-03-30 22:24:34 -0300

    finaliza programa

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Clear();
+
+string valorInicial;
+double valorSalario;
+double FGTS;
+
+Console.Write("Qual é o sálario que vamos calcular o FGTS? ");
+valorInicial = Console.ReadLine();
+
+valorSalario = Convert.ToDouble(valorInicial);
+
+FGTS = valorSalario * 0.08;
+
+
+
+Console.Write($"com o sálario de {valorSalario} o FGTS será de {FGTS}");
